790655f326d3495de3569055c0d20d27a632982c
Supapond <supapondmaliwab@gmail.com>
2025-09-03 14:20:41 +0700

    fix(ex-1and2.js): add code exercise 1 and 2

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,16 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const TotalPrice = 15000;
+const Totalrestaurant = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+TotalPrice = 5000;
+Totalrestaurant = 2500;
